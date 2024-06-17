51fb0c37f30d6ba5336c4d7d3badaffc3014ba2e
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-06-17 16:31:56 +0300

    Moving the dodger

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  } else if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  console.log(left);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  console.log(left);
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
